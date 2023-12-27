4e68a7a10cb30cf3b7408011ffebc5307ed05bb8
ZAHRA ELHAMRAOUI <50144165+zahrael97@users.noreply.github.com>
2023-12-27 11:28:05 +0100

    Create Grouping_mw_irt.py

---
+++ MSCI/Grouping_MS1/Grouping_mw_irt.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+from itertools import combinations
+
+def process_data(group):
+    edges_set = set()
+    for subgraph in group:
+        edges_sub_graph = combinations(subgraph, 2)
+        edges_set.update(edges_sub_graph)
+        print("processing_data_done")
+    return list(edges_set)
+
+
+class Grouping_mw_irt:
+    def __init__(self, data_frame):
+        self.df = data_frame
+        #self.df = self.df.drop_duplicates(subset=['Name'])
+        self.df['index'] = self.df.index
+        self.df = self.df.sort_values(by='MW')
+        self.df = self.df.reset_index(drop=True)
+    
+    def group_and_delete_duplicates(self, lst, tolerance1, unit='Da'):
+        if unit == 'ppm':
+            tolerance1 = (tolerance1 / 1e6) * lst[0]
+        groups = set()
+        indices = list(range(len(lst)))
+        indices.sort(key=lambda i: lst[i])
+        for i in range(len(lst)):
+            group = {indices[i]}
+            for j in range(i+1, len(lst)):
+                if lst[indices[j]] - lst[indices[i]] <= tolerance1:
+                    group.add(indices[j])
+                else:
+                    break
+            if not any(group.issubset(existing_group) for existing_group in groups):
+                groups.add(frozenset(group))
+        final_groups = [list(group) for group in groups]
+        return final_groups
+    
+    def grouper(self, iterable, tolerance2):
+        group = []
+        for item in iterable:
+            if not group or item - group[0] <= tolerance2:
+                group.append(item)
+            else:
+                yield group
+                group = [item]
+        if group:
+            yield group
+    
+    def irt_grouping(self, nested_list, tolerance2):
+        my_group = []
+        for k in range(len(nested_list)):
+            prev = None
+            group = []
+            df_mw = np.array([el[1] for el in nested_list[k]])
+            df_irt = np.array([el[2] for el in nested_list[k]])       
+            df_irt_index = np.array([int(el[3]) for el in nested_list[k]])
+            new_indexes = np.argsort(df_irt)
+            df_mw_sorted = df_mw[new_indexes]
+            df_irt_sorted = df_irt[new_indexes]
+            df_irt_index_sorted = df_irt_index[new_indexes]
+            group = list(self.grouper(df_irt_sorted, tolerance2))
+            iter_index = iter(df_irt_index_sorted)
+            result = [[next(iter_index) for _ in group] for group in group]
+            my_group.append(result)
+        return my_group
+    
+    def group_sequences(self, tolerance1, tolerance2, unit='Da'):
+        groups = self.group_and_delete_duplicates(self.df.iloc[:, 1].tolist(), tolerance1, unit=unit)
+        out = [[self.df.iloc[y].values.tolist() for y in x] for x in groups] 
+        group = self.irt_grouping(out, tolerance2)
+
+        fset = set()
+        for sub_list in group:
+            temp_set = set()
+            for item in sub_list:
+                temp_set.add(frozenset(item))
+            fset |= temp_set
+
+        group = sorted([list(x) for x in fset], key=lambda x: x[0])
+        d = {}
+        for i, l in enumerate(group):
+            for v in l:
+                d.setdefault(v, set()).add(i)
+
+        group = [l for i, l in enumerate(group) if not set.intersection(*(d[x] for x in l)).difference({i})]
+        print("Grouping Done")
+        return group
+
+
+
