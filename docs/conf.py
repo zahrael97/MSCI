9b7f422eded231099e394a74d545d75de73bf261
ZAHRA ELHAMRAOUI <50144165+zahrael97@users.noreply.github.com>
2023-12-29 15:11:35 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -85,6 +85,7 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_logo = 'https://github.com/zahrael97/MSCI/blob/master/docs/MSCI_logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a
