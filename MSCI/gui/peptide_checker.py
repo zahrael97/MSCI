9e402443901b2498af6c9ab090fa493ea8653c0a
ZAHRA ELHAMRAOUI <50144165+zahrael97@users.noreply.github.com>
2024-08-26 15:54:38 +0200

    Update peptide_checker.py

--- MSCI/gui/peptide_checker.py
+++ MSCI/gui/peptide_checker.py
@@ -7,13 +7,7 @@ from Bio import SeqIO
 import io
 
 DATASETS = {
-    "Reference Human Canonical proteome": {
-        25: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE25.csv",
-        28: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE28.csv",
-        30: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE30.csv",
-        32: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE32.csv",
-        35: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE35.csv"
-    },
+
 #    "Immunopeptidome": {
 #        28: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE28.csv",
 #    },
@@ -22,6 +16,14 @@ DATASETS = {
 #    }
     "Human Oral microbiome": {
         28: "https://github.com/zahrael97/MSCI/blob/master/Database/Oral_microbiom.csv",
+    },
+
+    "Reference Human Canonical proteome": {
+        25: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE25.csv",
+        28: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE28.csv",
+        30: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE30.csv",
+        32: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE32.csv",
+        35: "https://raw.githubusercontent.com/zahrael97/MSCI/master/Database/NSA_HRHR_NCE35.csv"
     }
 }
 
