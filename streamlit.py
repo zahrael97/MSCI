9442be5a95320593887df022e2dee3df8fa88df9
Zahra Elhamraoui <50144165+zahrael97@users.noreply.github.com>
2025-02-11 15:50:27 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,8 @@ from MSCI.gui.landing_page import landing_page
 from MSCI.gui.peptide_analysis import peptide_twins_analysis, plot_spectra
 from MSCI.gui.peptide_checker import peptide_twins_checker
 
+
+
 def main():
     st.set_page_config(layout="wide")
     add_custom_css()
@@ -15,31 +17,22 @@ def main():
     logo_url = "https://github.com/proteomicsunitcrg/MSCI/raw/main/docs/MSCI_logor.png"
     logo_image = load_image_from_url(logo_url)
 
-    # Default selection
-    if "selected_option" not in st.session_state:
-        st.session_state.selected_option = "MSCI"
-
     with st.sidebar:
         if logo_image:
-            if st.button("", key="logo_button"):
-                st.session_state.selected_option = "MSCI"
             st.markdown(f"""
             <p align="center">
                 <img src="data:image/png;base64,{logo_image}" alt="logo" width="300" height="300">
             </p>
             """, unsafe_allow_html=True)
-        
         st.header("MSCI")
-        option = st.radio("Choose an option", ("Peptide Twins Analysis", "Peptide Twins Checker"))
-        st.session_state.selected_option = option
-    
-    # Page navigation
-    if st.session_state.selected_option == "MSCI":
+        option = st.radio("Choose an option", ("MSCI", "Peptide Twins Analysis", "Peptide Twins Checker"))
+
+    if option == "MSCI":
         landing_page()
-    elif st.session_state.selected_option == "Peptide Twins Analysis":
+    elif option == "Peptide Twins Analysis":
         peptide_twins_analysis()
         plot_spectra()
-    elif st.session_state.selected_option == "Peptide Twins Checker":
+    elif option == "Peptide Twins Checker":
         peptide_twins_checker()
 
 if __name__ == "__main__":
