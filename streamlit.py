a9aeffdf12163b32ebedccac356cb84954c55cf9
Zahra Elhamraoui <50144165+zahrael97@users.noreply.github.com>
2025-02-11 15:35:17 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,10 +16,12 @@ def main():
     logo_url = "https://github.com/proteomicsunitcrg/MSCI/raw/main/docs/MSCI_logor.png"
     logo_image = load_image_from_url(logo_url)
 
+    landing_page_displayed = st.session_state.get("landing_page_displayed", True)
+
     with st.sidebar:
         if logo_image:
             if st.button("", key="logo_button"):
-                landing_page()
+                st.session_state["landing_page_displayed"] = True
             st.markdown(f"""
             <p align="center">
                 <a href="#" onclick="window.location.reload();">
@@ -30,12 +32,15 @@ def main():
         st.header("MSCI")
         option = st.radio("Choose an option", ("Peptide Twins Analysis", "Peptide Twins Checker"))
 
-    # Render selected option only
-    if option == "Peptide Twins Analysis":
-        peptide_twins_analysis()
-        plot_spectra()
-    elif option == "Peptide Twins Checker":
-        peptide_twins_checker()
+    if landing_page_displayed:
+        landing_page()
+        st.session_state["landing_page_displayed"] = False
+    else:
+        if option == "Peptide Twins Analysis":
+            peptide_twins_analysis()
+            plot_spectra()
+        elif option == "Peptide Twins Checker":
+            peptide_twins_checker()
 
 if __name__ == "__main__":
     main()
