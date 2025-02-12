2688947acc69d8f451a070dfb6e6e840fd40f8a6
Zahra Elhamraoui <50144165+zahrael97@users.noreply.github.com>
2025-02-12 15:20:16 +0100

    Update main.py

--- MSCI/gui/main.py
+++ MSCI/gui/main.py
@@ -10,33 +10,20 @@ def main():
     st.set_page_config(layout="wide")
     add_custom_css()
 
-    # Initialize session state
-    if "page" not in st.session_state:
-        st.session_state.page = "Landing"
-
     logo_path = Path("images/MSCI_logor.png")
     logo_image = load_image(str(logo_path))
 
     with st.sidebar:
         if logo_image:
-            # Make the image clickable by wrapping it in a hyperlink
             st.markdown(f"""
             <p align="center">
-                <a href="?page=Landing">
-                    <img src="data:image/png;base64,{logo_image}" alt="logo" width="300" height="300">
-                </a>
+                <img src="data:image/png;base64,{logo_image}" alt="logo" width="300" height="300">
             </p>
             """, unsafe_allow_html=True)
-
         st.header("MSCI")
-        option = st.radio("Choose an option", ("Peptide Twins Analysis", "Peptide Twins Checker"))
+        option = st.radio("Choose an option", ("MSCI", "Peptide Twins Analysis", "Peptide Twins Checker"))
 
-    # Handle page navigation
-    query_params = st.experimental_get_query_params()
-    if "page" in query_params and query_params["page"][0] == "Landing":
-        st.session_state.page = "Landing"
-
-    if st.session_state.page == "Landing":
+    if option == "MSCI":
         landing_page()
     elif option == "Peptide Twins Analysis":
         peptide_twins_analysis()
