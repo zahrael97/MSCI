40dae46d8245c959924b27b7fc121d92b15e9c28
Zahra Elhamraoui <50144165+zahrael97@users.noreply.github.com>
2025-04-17 10:23:03 +0200

    Update landing_page.py

--- MSCI/gui/landing_page.py
+++ MSCI/gui/landing_page.py
@@ -41,17 +41,17 @@ def landing_page():
     """)
 
 
-st.subheader("Usage")
+    st.subheader("Usage")
 
-st.write("""
-Example workflow: Visit [MSCI Tutorial](https://msci.readthedocs.io/en/latest/tutorial.html)
+    st.write("""
+    Example workflow: Visit [MSCI Tutorial](https://msci.readthedocs.io/en/latest/tutorial.html)
 
-For a full tutorial, visit our Colab notebook:  
-[Colab Notebook](https://colab.research.google.com/drive/1ny97RNgvnpD7ZrHW8TTRXWCAQvIcavkk)
+    For a full tutorial, visit our Colab notebook:  
+    [Colab Notebook](https://colab.research.google.com/drive/1ny97RNgvnpD7ZrHW8TTRXWCAQvIcavkk)
 
-For code and datasets, visit our GitHub repo:  
-[GitHub - proteomicsunitcrg/MSCI](https://github.com/proteomicsunitcrg/MSCI)
-""")
+    For code and datasets, visit our GitHub repo:  
+    [GitHub - proteomicsunitcrg/MSCI](https://github.com/proteomicsunitcrg/MSCI)
+    """)
 
 
 
